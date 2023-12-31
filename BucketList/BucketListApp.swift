2a42850f5d575826898d4513342c7608b2135f4e
Henny2 <henrieke.baunack@web.de>
2023-12-31 13:32:12 -0800

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Henrieke Baunack on 12/31/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
