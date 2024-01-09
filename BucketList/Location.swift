517d3604180e1a3e6a518ed8c93eb8247d74ef8e
Henny2 <henrieke.baunack@web.de>
2024-01-08 20:01:04 -0800

    changing annotation and changing up data structure

--- BucketList/Location.swift
+++ BucketList/Location.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import MapKit
 
 
 struct Location: Codable, Identifiable, Equatable {
@@ -14,4 +15,20 @@ struct Location: Codable, Identifiable, Equatable {
     var description: String
     var latitude: Double // storing these as a double to be able to use them with codable
     var longitute: Double
+    
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitute)
+    }
+    
+    // wtih this if else, it won't be shipped to the users app/phone
+    #if DEBUG
+    static let example = Location(id: UUID(), name: "Buckingham Palace", description: "Lit by a lot of lightbulbs", latitude: 51.501, longitute: -0.141)
+    #endif
+    
+    
+    // making own func for Equatable so that it does not compare all the properties bc we know if the ids are not the same, they have to be different
+    static func ==(lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
+    
 }
